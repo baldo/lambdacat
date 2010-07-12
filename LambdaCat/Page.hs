8205dc0a81897b687807b55d62f85c780a3a0b9d
Andreas Baldeau <andreas@baldeau.net>
2010-07-12 22:56:00 +0200

    DIRTY page change on demand.

--- LambdaCat/Page.hs
+++ LambdaCat/Page.hs
@@ -4,13 +4,14 @@ module LambdaCat.Page
     ( Page (..)
     , PageClass (..)
     , HasWidget (..)
-    , pageFromURI 
+    , pageFromProtocol
     )
 where
 
 import LambdaCat.Protocol
 
 import Control.Monad.Trans
+import Data.Typeable
 import Network.URI
 import Graphics.UI.Gtk.Abstract.Widget
 
@@ -21,7 +22,7 @@ class MonadIO m => PageClass page m where
     -- | Some uri functions
     load :: page -> URI -> m ()
 
-    -- | 
+    -- |
     back, forward, stop, reload :: page -> m ()
     back _ = return ()
     forward _ = return ()
@@ -36,29 +37,50 @@ class MonadIO m => PageClass page m where
 class WidgetClass w => HasWidget hw w | hw -> w where
     getWidget :: hw -> w
 
-data Page m = forall hw w . (HasWidget hw w, PageClass hw m) => Page hw
+data Page m = forall hw w . (Typeable hw, HasWidget hw w, PageClass hw m) => Page hw
 
 instance MonadIO m => PageClass (Page m) m where
-   new = return (error "Can't create existential quantificated datatype")  
+   new = return (error "Can't create existential quantificated datatype")
 
    load (Page p) = load p
-   
+
    back (Page p) = back p
    forward (Page p) = forward p
    stop (Page p) = stop p
    reload (Page p) = reload p
-   
+
    getBackHistory (Page p) = getBackHistory p
    getForwardHistory (Page p) = getForwardHistory p
 
-pageFromURI :: (PageClass page m,MonadIO m) => [((m page),[Protocol])] -> (Maybe URI) -> m (Maybe page)
-pageFromURI _ Nothing = return Nothing
-pageFromURI pList (Just uri) = do
-    let schema = uriScheme uri
-        pages  = map (\ (f,_) -> f) $ filter (\ (_,protos) -> hasProtocol schema protos) pList 
-    if null pages 
-     then return Nothing
-     else do page <- (head pages)
-             load page uri
-             return (Just page)
- where hasProtocol prot = not . null . filter (== prot) 
+eqType :: (Typeable a, Typeable b) => a -> b -> Bool
+eqType a b = typeOf a == typeOf b
+
+eqPageType :: Page m1 -> Page m2 -> Bool
+eqPageType (Page p1) (Page p2) = p1 `eqType` p2
+
+createPage :: (MonadIO m, PageClass p m) => p -> m p
+createPage _ = new
+
+pageFromProtocol :: MonadIO m => [(Page m, [Protocol])] -> Maybe (Page m) -> Maybe URI -> m (Maybe (Page m))
+pageFromProtocol _  _  Nothing    = return Nothing
+pageFromProtocol [] _  _          = return Nothing
+pageFromProtocol ps mp (Just uri) = do
+    mp' <- lookupProtocol ps
+
+    case (mp, mp') of
+        (_,       Nothing       ) -> return Nothing
+        (Nothing, Just (Page p')) -> createPage p' >>= return . Just . Page
+        (Just (Page p), Just (Page p'))
+            | p `eqType` p' -> return mp
+            | otherwise     -> createPage p' >>= return . Just . Page
+
+    where
+        protocol = uriScheme uri
+
+        lookupProtocol :: MonadIO m => [(Page m, [Protocol])] -> m (Maybe (Page m))
+        lookupProtocol [] = return Nothing
+        lookupProtocol ((page, protos) : plist)
+            | protocol `elem` protos = do
+                return $ Just page
+            | otherwise = lookupProtocol plist
+
