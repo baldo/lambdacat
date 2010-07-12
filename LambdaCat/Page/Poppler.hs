e815886dee3abc37233bb7ba27005dd5510470ac
Daniel Ehlers <danielehlers@mindeye.net>
2010-07-12 12:37:59 +0200

    Add page layout algorithm

--- LambdaCat/Page/Poppler.hs
+++ LambdaCat/Page/Poppler.hs
@@ -4,14 +4,14 @@ module LambdaCat.Page.Poppler
     ( PopplerPage
     ) where
 
-import LambdaCat.Page
+import LambdaCat.Page hiding (Page)
 
 import Control.Concurrent
 import Control.Monad.Trans
 import System.Glib.GObject
-import Graphics.UI.Gtk
+import Graphics.UI.Gtk hiding (Point)
 import Graphics.UI.Gtk.Poppler.Document hiding (PageClass)
-import Graphics.UI.Gtk.Poppler.Page
+import Graphics.UI.Gtk.Poppler.Page 
 import Graphics.UI.Gtk.Abstract.Object
 import Network.URI
 import Data.Maybe
@@ -25,6 +25,15 @@ data PopplerPage = PopplerPage
     , pageDocument :: MVar (Maybe Document)
     , pageNumber   :: MVar Int 
     }
+
+type CurrentPage = Int
+type Point = (Double,Double)
+type PagePositioner = Document  -- ^ Document that should be layouted
+                    -> CurrentPage -- ^ Page which must be visible 
+                    -> Int -- ^ Number of pages shown
+                    -> Point -- ^ Size of the Screen
+                    -> IO [(Page,Double,Point,Point)] -- ^ Where should which page be drawn
+
 
 -- The following is probably a bad idea, maybe it throws an LALALALA error.
 -- There is no documented way to instantiate a widget....
@@ -63,29 +72,38 @@ instance MonadIO m => PageClass PopplerPage m where
         doc <- liftM (fromMaybe (error "Error opening pdf file.")) (documentNewFromFile (uriString) Nothing)
         _ <-  takeMVar mdoc
         putMVar mdoc (Just doc)
+        widgetQueueDraw area 
      where uriString = uriToString id uri ""
 
 viewerDraw :: PopplerPage -> EventM EExpose ()
 viewerDraw viewer = do
   let area = pageArea viewer
+      scroll = pageScrollable viewer
   pN       <- liftIO $ readMVar $ pageNumber viewer
-  mayBeDoc <- liftIO $ readMVar $ pageDocument viewer-- TODO replace with takeMVar
-  (winWidth, winHeight) <- eventWindowSize                    
+  mayBeDoc <- liftIO $ takeMVar $ pageDocument viewer-- TODO replace with takeMVar
+  (winWidth, winHeight) <- liftIO $ widgetGetSize scroll
   case mayBeDoc of
     Nothing -> return ()
-    (Just doc) ->  liftIO $ do
-        page       <- documentGetPage doc pN
-        frameWin   <- widgetGetDrawWindow area
-        (docWidth, docHeight) <- pageGetSize page
-        widgetSetSizeRequest area (truncate docWidth) (truncate docHeight)
+    (Just doc) -> liftIO $ do
+        frameWin  <- widgetGetDrawWindow area 
 
-        renderWithDrawable frameWin $ do 
+        positions <- fitPage doc pN 2 (fromIntegral winWidth,fromIntegral winHeight)
+        let widgetWidth = foldr (\ (_,_,(x0,_),(w,_)) m -> m `max` (x0 + w)) 0 positions
+        let widgetHeight = foldr (\ (_,_,(_,y0),(_,h)) m -> m `max` (y0 + h)) 0 positions
+        widgetSetSizeRequest area (truncate widgetWidth) (truncate widgetHeight)
+
+        mapM_ (\ (page,scal,(x0,y0),(x1,y1)) -> renderWithDrawable frameWin $ do 
             setSourceRGB 1.0 1.0 1.0
-            rectangle 0.0 0.0 winWidth winHeight
+            rectangle x0 y0 x1 y1 
             fill
-            let scaleX = winWidth / docWidth 
-            scale scaleX scaleX
+            translate x0 y0
+            scale scal scal
             pageRender page
+            ) $ positions
+  liftIO $ putMVar (pageDocument viewer) mayBeDoc
+
+
+    
 
 eventWindowSize :: EventM EExpose (Double, Double)      
 eventWindowSize = do
@@ -94,3 +112,26 @@ eventWindowSize = do
     return $ if w * h > 1
                then (fromIntegral w, fromIntegral h)
                else (1,1)
+
+
+fitPage :: PagePositioner 
+fitPage doc currentPage count (winWidth,winHeight) = do
+    numOfPages <- documentGetNPages doc
+    let cPosNum   = currentPage `mod` count
+        firstPage = currentPage - cPosNum
+        lastPage  = firstPage + count - 1
+        pageSpace = (fromIntegral $ floor $ winWidth / fromIntegral count)  :: Double 
+    pages <- mapM (\num -> documentGetPage doc num) [firstPage..lastPage]
+    foldM (\ lst page -> do
+            (pageWidth,pageHeight) <- pageGetSize page
+            let leftSide  = (fromIntegral $ length lst )  * pageSpace
+                rightSide = leftSide + pageSpace `min` winWidth
+                drawHeight = if leftSide + (winHeight / pageHeight) * pageWidth > rightSide
+                             then (pageSpace / pageWidth) * pageHeight
+                             else winHeight - 5
+                scaleX = drawHeight / pageHeight
+                drawWidth = pageWidth * scaleX
+                x0 = fromIntegral $  0 `max` (floor $ (pageSpace - drawWidth) / 2)
+                y0 = fromIntegral $  0 `max` (floor $ (winHeight - drawHeight) / 2)
+            return $ (page,scaleX,(x0 + leftSide,y0),(drawWidth,drawHeight)):lst
+         ) [] pages
