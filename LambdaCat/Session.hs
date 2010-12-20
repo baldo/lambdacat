fef493ccfd84b99c7d306a45e9751e98bc2127a6
Daniel Ehlers <danielehlers@mindeye.net>
2010-12-20 18:03:50 +0100

    Implement getTab function

--- LambdaCat/Session.hs
+++ LambdaCat/Session.hs
@@ -2,8 +2,8 @@ module LambdaCat.Session where
 
 import LambdaCat.History (History)
 import qualified LambdaCat.History as History 
-import Data.IntMap (IntMap)
-import qualified Data.IntMap as IntMap
+import Data.Map (Map)
+import qualified Data.Map as Map
 import Network.URI (URI) 
 
 data Session tabIdent tabmeta = Session 
@@ -20,7 +20,7 @@ data Tab tabIdent tabmeta  = Tab
 -- | create a new session with given session meta data.
 newSession :: Session tabIdent tabMeta 
 newSession = Session 
-  { sessionTabs = IntMap.empty
+  { sessionTabs = Map.empty
   , sessionTabActive = Nothing
   }
 
@@ -29,30 +29,28 @@ newTab :: Ord tabIdent
        => tabIdent 
        -> tabMeta 
        -> URI 
-       -> Session sessionMeta tabIdent tabMeta 
-       -> (Int,Session sessionMeta tabIdent tabMeta)
+       -> Session tabIdent tabMeta 
+       -> Session tabIdent tabMeta
 newTab ti tm uri session = 
   let tab   = Tab { tabIdent = ti , tabHistory = History.singleton uri , tabMeta = tm  }
       sessTabs = sessionTabs session
-      index    = newIndex sessTabs
-  in  (index,session { sessionTabs = IntMap.insert index tab sessTabs })
+  in  session { sessionTabs = Map.insert ti tab sessTabs }
 
-newIndex :: IntMap a -> Int 
-newIndex m | IntMap.null m  = 0
-           | otherwise      = 1 + (fst $ IntMap.findMax m)
 
 -- | Execute action on tab with ident in session
 updateTab :: (Eq tabIdent,Ord tabIdent)
-          => Session sessionMeta tabIdent tabMeta 
+          => Session tabIdent tabMeta 
           -> tabIdent                   -- ^ ident of tab to be modified
           -> (tabMeta -> Maybe tabMeta) -- ^ if nothing is returned the tab gets deleted
-          -> Session sessionMeta tabIdent tabMeta
+          -> Session tabIdent tabMeta
 updateTab session tabId f =
   let tabs    = sessionTabs session
-      newTabs = IntMap.updateWithKey search tabId tabs -- TODO fix 
+      newTabs = Map.updateWithKey search tabId tabs -- TODO fix 
       search  = \ _ tab -> do meta <- f (tabMeta tab)
                               return $ tab { tabMeta = meta }
   in  session { sessionTabs = newTabs } 
 
-getTab :: Session tabIdent tabMeta -> tabIdent -> Maybe (Tab tabIdent tabMeta)
--- TODO implement
+getTab :: Ord tabIdent => Session tabIdent tabMeta -> tabIdent -> Maybe (Tab tabIdent tabMeta)
+getTab session ti = 
+  let sessTabs = sessionTabs session
+  in  Map.lookup ti sessTabs 
