737153578782c6d76eadf6b1b572b23f6f5ce5b7
Andreas Baldeau <andreas@baldeau.net>
2010-12-21 14:09:15 +0100

    Really added LambdaCat.UI.

---
+++ LambdaCat/UI.hs
@@ -0,0 +1,16 @@
+module LambdaCat.UI
+    ( module LambdaCat.Class
+    , module LambdaCat.View
+    )
+where
+
+import LambdaCat.Class
+    ( UIClass (..)
+    , Callback
+    )
+
+import LambdaCat.View
+    ( ViewClass (..)
+    , View (..)
+    )
+
