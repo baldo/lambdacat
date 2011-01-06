4ef5cc4a30d38cb9e11c4c8287a9aa1c066303e0
Andreas Baldeau <andreas@baldeau.net>
2011-01-06 22:53:14 +0100

    Added documentation to Main.hs.

--- Main.hs
+++ Main.hs
@@ -1,6 +1,11 @@
+-- | This is the main module for lambdacat's global binary. Its purpose is
+-- (re-)compiling the config file (real main module) and running the
+-- generated binary.
+
 module Main
     ( main
-    ) where
+    )
+where
 
 import LambdaCat
 
