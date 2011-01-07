7017438827c0d09f2af4be130f3aa435d38f63f9
Andreas Baldeau <andreas@baldeau.net>
2011-01-07 17:37:28 +0100

    Documentation header and formatting for UI.hs.

--- LambdaCat/UI.hs
+++ LambdaCat/UI.hs
@@ -1,3 +1,14 @@
+-- |
+-- Module      : LambdaCat.UI
+-- Copyright   : Andreas Baldeau, Daniel Ehlers
+-- License     : BSD3
+-- Maintainer  : Andreas Baldeau <andreas@baldeau.net>,
+--               Daniel Ehlers <danielehlers@mindeye.net>
+-- Stability   : Alpha
+--
+-- This module exports the relevant typeclasses and datastructures to
+-- implement your own UI for lambdacat.
+
 module LambdaCat.UI
     ( module LambdaCat.Internal.Class
     , module LambdaCat.View
@@ -8,7 +19,6 @@ import LambdaCat.Internal.Class
     ( UIClass (..)
     , Callback
     )
-
 import LambdaCat.View
     ( ViewClass (..)
     , View (..)
